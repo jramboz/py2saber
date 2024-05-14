fc6cbffe60791eb1c99d748110cca787f48b578a
Jason Ramboz <jramboz@gmail.com>
2024-05-14 10:51:35 -0400

    Bump version: 0.15.4 → 0.15.5

--- py2saber/__init__.py
+++ py2saber/__init__.py
@@ -3,4 +3,4 @@ from .py2saber import NoAnimaSaberException, NotEnoughFreeSpaceException, AnimaF
 
 __all__ = ["py2saber"]
 
-__version__ = "0.15.4"
+__version__ = "0.15.5"
--- py2saber/py2saber.py
+++ py2saber/py2saber.py
@@ -28,7 +28,7 @@ import time
 
 basedir = os.path.dirname(os.path.realpath(__file__))
 
-script_version = '0.15.4'
+script_version = '0.15.5'
 script_authors = 'Jason Ramboz'
 script_repo = 'https://github.com/jramboz/py2saber'
 
